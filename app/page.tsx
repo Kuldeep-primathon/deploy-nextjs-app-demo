a91397c0972a3defe1556359940dff7379b16a33
Kuldeep-primathon <kuldeep.chahar@primathon.in>
2024-03-11 15:23:12 +0530

    simple home page and about page

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (<><h1 className="text-4xl  m-auto w-full text-center">Home page</h1>
+  <div className=" flex justify-center items-center al h-lvh">
+    <p className="text-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ipsam expedita laborum. Consectetur mollitia nesciunt, assumenda aliquam qui nisi ex optio non sunt eos dolore maiores repellendus pariatur. Modi, explicabo.</p>
+    </div></>
+    
+    
+  );
+}
